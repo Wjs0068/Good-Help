ce52bedde9c22cf8bd9ca466d3d2ff441f40ba92
Wjs0068 <87343895+Wjs0068@users.noreply.github.com>
2022-06-13 20:42:11 -0700

    Add additional questions to form

--- src/components/AddBrand.js
+++ src/components/AddBrand.js
@@ -1,36 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import "./AddBrand.css";
+import Stack from "@mui/material/Stack";
 
 function AddBrand() {
   const type = [{ label: "Retail" }, { label: "Service" }];
 
+  const serviceOptions = [
+    { title: "Home", option: "Movers" },
+    { title: "Home", option: "Electrician" },
+    { title: "Home", option: "HVAC" },
+    { title: "Home", option: "Lawn Maintenance" },
+    { title: "Home", option: "Plumbing" },
+    { title: "Home", option: "Home Improvements" },
+    { title: "Home", option: "Paint" },
+    { title: "Auto", option: "Maintenance" },
+    { title: "Auto", option: "Body Repair" },
+    { title: "Auto", option: "Detailing" },
+    { title: "Financial", option: "Financial Planning" },
+    { title: "Financial", option: "Insurance" },
+    { title: "Financial", option: "Tax and Accounting" },
+  ];
+  const retailOptions = [
+    { title: "Toys", option: "Lego" },
+    { title: "Toys", option: "fffff" },
+    { title: "Toys", option: "Lego" },
+    { title: "Toys", option: "Lego" },
+    { title: "Toys", option: "Lego" },
+  ];
+
   return (
     <form className="brand-form">
-      <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={type}
-        sx={{ width: 300 }}
-        renderInput={(params) => (
-          <TextField {...params} label="Type of Business" />
-        )}
-      />
-      
-      <Autocomplete
-        multiple
-        id="tags-outlined"
-        options={type}
-        getOptionLabel={(option) => option.title}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="filterSelectedOptions"
-            placeholder="Favorites"
-          />
-        )}
-      />
+      <Stack spacing={3} sx={{ width: 500 }}>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={type}
+          sx={{ width: 500 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Type of Business" />
+          )}
+        />
+        <Autocomplete
+          multiple
+          id="tags-outlined"
+          options={serviceOptions}
+          getOptionLabel={(option) => option.option}
+          groupBy={(serviceOptions) => serviceOptions.title}
+          sx={{ width: 500 }}
+          filterSelectedOptions
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="filterSelectedOptions"
+              placeholder="Favorites"
+            />
+          )}
+        />
+      </Stack>
     </form>
   );
 }
